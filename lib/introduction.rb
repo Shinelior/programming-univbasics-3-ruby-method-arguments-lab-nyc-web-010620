bfdb5b985037c9d5b1f6e89f526d14ec4ac08972
lior Shine <shinelior@gmail.com>
2019-12-10 07:39:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,7 @@ def introduction (name)
   
 end
 
-def ntroduction_with_language(name,language)
+def introduction_with_language(name,language)
   "Hi, my name is #{name} and I am learning to program in #{language}."
   
 end
@@ -20,7 +20,7 @@ def introduction_with_language_optional(name,language="Ruby")
 end
 
 introduction ("Josh")  
-introduction (name)
+introduction ("Dan")
 
 
 
